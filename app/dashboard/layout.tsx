060e442aca89ce6791639c5f8528d57d63751a6d
Richard Lipa <rlipac31@gmail.com>
2026-01-13 17:39:48 -0500

    corrigendo layout 2

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -2,7 +2,6 @@
 
 import { UserProvider } from '../../context/UserContext';
 import { cookies } from 'next/headers';
-import { UserData}  from '../../types';
 
 import Sidebar from '../components/Sidebar';
 
@@ -13,26 +12,34 @@ export default async function DashboardLayout({
 }) {
 
 
-    const cookieStore = await cookies();
+  const cookieStore = await cookies();
+
+  interface UserData {
+    idUser: number | string;
+    name: string;
+    role: string;
+    token: string;
+  }
+
 
   // Preparamos los datos iniciales desde el servidor
 
-const initialUser: UserData = {
-  idUser: '',
-  name: '',
-  role: '',
-  token: cookieStore.get('session_token')?.value || '' // Supongamos que obtienes el token de cookies o una variable
-};
-  
- 
+  const initialUser: UserData = {
+    idUser: '',
+    name: '',
+    role: '',
+    token: cookieStore.get('session_token')?.value || '' // Supongamos que obtienes el token de cookies o una variable
+  };
+
+
   return (
     <UserProvider initialUser={initialUser}>
-        <div className="flex">
+      <div className="flex">
         <Sidebar />
         <div className="ml-64 w-full min-h-screen bg-gray-50 p-8"> {/* ml-64 para dejar espacio al sidebar */}
-            {children}
+          {children}
         </div>
-        </div>
-   </UserProvider>   
+      </div>
+    </UserProvider>
   );
 }
